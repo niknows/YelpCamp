e0c52de28748ab3805e698e9d9cad51bfbf2c52f
niknows <33270711+niknows@users.noreply.github.com>
2018-07-13 22:00:09 -0300

    Add files via upload

--- v2/app.js
+++ v2/app.js
@@ -18,21 +18,21 @@ var campgroundSchema = new mongoose.Schema({
 
 var Campground = mongoose.model("Campground", campgroundSchema) ;
 
-// var newCampground = new Campground({
+//  var newCampground = new Campground({
     
-//   name: "Vast Paradise Canyon",
-//   image: "https://farm6.staticflickr.com/5608/15344614880_60b9be5b4f.jpg"
+//   name: "Snow Montain",
+//   image: "https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
    
-// });
+//  });
 
-// newCampground.save(function(err,campground){
-//     if(err){
-//         console.log("Something went wrong");
-//     } else{
-//         console.log("Success!!");
-//         console.log(campground);
-//     }
-// });
+//  newCampground.save(function(err,campground){
+//      if(err){
+//          console.log("Something went wrong");
+//      } else{
+//          console.log("Success!!");
+//          console.log(campground);
+//      }
+//  });
 
 /*ROUTES*/
 app.get("/",function(req,res){
@@ -41,7 +41,7 @@ app.get("/",function(req,res){
 app.get("/campgrounds", function(req,res){
     Campground.find({},function(err,allCampgrounds){
         if(err){
-            console.log("Error...");
+            console.log("Error");
         }else{
             res.render("campgrounds",{campgrounds:allCampgrounds}); 
         }
@@ -54,8 +54,19 @@ app.get("/campgrounds/new",function(req,res){
 app.post("/campgrounds", function(req,res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
+    //Create a new campground and add it to the database
+    var newCampground = new Campground({
+        name: name,
+        image: image
+    });
+    newCampground.save(function(err,campground){
+       if(err){
+           console.log("Something went wrong");
+       } else{
+           console.log("The campground was added to the datavase");
+           console.log(campground);
+       } 
+    });
     res.redirect("/campgrounds");
     
 });
