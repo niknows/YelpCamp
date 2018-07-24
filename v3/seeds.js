67201a97f94da8a0d56f59b7b8c55fb73af4796f
niknows <33270711+niknows@users.noreply.github.com>
2018-07-24 18:41:26 -0300

    Add files via upload

--- v3/seeds.js
+++ v3/seeds.js
@@ -2,12 +2,14 @@ var mongoose = require('mongoose');
 var Campground = require('./models/campground');
 
 function seedDB(){
+    //Remove all campgrounds
     Campground.remove({}, function(err){
         if(err){
             console.log(err);
         }
         console.log("Removed campgrounds!!!");
     });
+    //Add campgrounds
 }
 
 module.exports = seedDB;
